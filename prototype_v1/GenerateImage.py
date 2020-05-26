ecb03072f1e2c4f009008396d807930cf64698a4
rizkikaZakka <rizkika.palindungan1@gmail.com>
2020-05-26 17:38:54 +0700

    Update GenerateImage.py

--- prototype_v1/GenerateImage.py
+++ prototype_v1/GenerateImage.py
@@ -14,15 +14,62 @@ Cara Running : - python gather_images.py <nama_label> <jumlah_sampel>
 import cv2  # image processing
 import os  # memanipulasi operating system
 import sys  # menangkap arguments/parse
+import numpy as np
 
 
 class ImageProcessing:
     def __init__(self, num_samples, IMG_CLASS_PATH):
         self.num_samples = num_samples
         self.IMG_CLASS_PATH = IMG_CLASS_PATH
+        self.window_name_tracking = 'Tracking'
+
+    def nothing(self, x):
+        pass
+
+    def hsv_boundary_tracker(self):
+
+        cv2.namedWindow(self.window_name_tracking)  # membuat window dengan nama
+
+        cv2.createTrackbar('LH', self.window_name_tracking, 0, 255, self.nothing)  # membuat trackbar
+        cv2.createTrackbar('LS', self.window_name_tracking, 0, 255, self.nothing)  # membuat trackbar
+        cv2.createTrackbar('LV', self.window_name_tracking, 0, 255, self.nothing)  # membuat trackbar
+
+        cv2.createTrackbar('UH', self.window_name_tracking, 255, 255, self.nothing)  # membuat trackbar
+        cv2.createTrackbar('US', self.window_name_tracking, 255, 255, self.nothing)  # membuat trackbar
+        cv2.createTrackbar('UV', self.window_name_tracking, 255, 255, self.nothing)  # membuat trackbar
 
     def image_manipulation(self, frame, count):
-        roi = frame[100:500, 100:500]  # img[y:y+h, x:x+w]
+        roi = frame[100:500, 100:500]  # img[y:y+h, x:x+w] --> crop image
+
+        hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)  # konversi warna dari BGR -> HSV
+
+        lower_h = cv2.getTrackbarPos('LH', self.window_name_tracking)  # mendapatkan posisi trackbar
+        lower_s = cv2.getTrackbarPos('LS', self.window_name_tracking)  # mendapatkan posisi trackbar
+        lower_v = cv2.getTrackbarPos('LV', self.window_name_tracking)  # mendapatkan posisi trackbar
+
+        upper_h = cv2.getTrackbarPos('UH', self.window_name_tracking)  # mendapatkan posisi trackbar
+        upper_s = cv2.getTrackbarPos('US', self.window_name_tracking)  # mendapatkan posisi trackbar
+        upper_v = cv2.getTrackbarPos('UV', self.window_name_tracking)  # mendapatkan posisi trackbar
+
+        # define range of skin color in HSV
+        lower_skin = np.array([lower_h, lower_s, lower_v], dtype=np.uint8)
+        upper_skin = np.array([upper_h, upper_s, upper_v], dtype=np.uint8)
+
+        # extract skin colur image
+        mask = cv2.inRange(hsv, lower_skin, upper_skin)
+
+        # # extrapolate the hand to fill dark spots within
+        # kernel = np.ones((3, 3), np.uint8)
+        # mask = cv2.dilate(mask, kernel, iterations=4)
+        #
+        # # blur the image
+        # mask = cv2.GaussianBlur(mask, (5, 5), 100)
+
+        res = cv2.bitwise_and(roi, roi, mask=mask)  # hasil akhir operasi gambar dengan mask
+
+        cv2.imshow('mask', mask)
+        cv2.imshow('res', res)
+
         # save_path = os.path.join(self.IMG_CLASS_PATH, '{}.jpg'.format(count + 1))
         # cv2.imwrite(save_path, roi)  # menyimpan file dataset satu persatu
 
@@ -34,6 +81,8 @@ class ImageProcessing:
         start = False
         count = 0
 
+        self.hsv_boundary_tracker()
+
         while cap.isOpened():
             ret, frame = cap.read()
 
@@ -42,8 +91,8 @@ class ImageProcessing:
                 continue  # semua proses akan melewati(skip) dan kembali ke perulangan selanjutnya
 
             # validasi jika jumlah dataset sudah memenuhi kriteria yang diinginkan
-            if count == self.num_samples:
-                break  # keluar dari proses perulangan
+            # if count == self.num_samples:
+            #     break  # keluar dari proses perulangan
 
             cv2.rectangle(frame, (100, 100), (500, 500), (255, 255, 255), 2)  # membuat bangun segi empat pada frame
 
