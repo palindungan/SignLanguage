9a97ec43e6f1c7b3d5494812378ef49fc4f576b9
rizkikaZakka <rizkika.palindungan1@gmail.com>
2020-05-26 23:46:26 +0700

    Update create_data_model.py

--- prototype_v1/create_data_model.py
+++ prototype_v1/create_data_model.py
@@ -9,26 +9,88 @@ import tensorflow as tf
 import os
 
 
+class TrainingDataset:
+
+    def __init__(self, IMG_SAVE_PATH, CLASS_MAP, NUM_CLASSES):
+        self.IMG_SAVE_PATH = IMG_SAVE_PATH
+        self.CLASS_MAP = CLASS_MAP
+        self.NUM_CLASSES = NUM_CLASSES
+
+    def mapper(self, val):
+        return self.CLASS_MAP[val]
+
+    def get_model(self):
+        model = Sequential([
+            SqueezeNet(input_shape=(227, 227, 3), include_top=False),
+            Dropout(0.5),
+            Convolution2D(self.NUM_CLASSES, (1, 1), padding='valid'),
+            Activation('relu'),
+            GlobalAveragePooling2D(),
+            Activation('softmax')
+        ])
+        return model
+
+
 def main():
     IMG_SAVE_PATH = 'image_data'  # lokasi dataset
-
     CLASS_MAP = {
-        'nol': 0,
-        'satu': 1,
-        'dua': 2,
-
+        'A': 0,
+        'B': 1,
+        'C': 2,
+        'D': 3,
+        'E': 4,
+        'F': 5
     }
+    NUM_CLASSES = len(CLASS_MAP)
 
-    # IMG_SAVE_PATH = 'image_data'
-    #
-    # CLASS_MAP = {
-    #     "rock": 0,
-    #     "paper": 1,
-    #     "scissors": 2,
-    #     "none": 3
-    # }
-    #
-    # NUM_CLASSES = len(CLASS_MAP)
+    TrainingDataset(IMG_SAVE_PATH, CLASS_MAP, NUM_CLASSES)
+
+    # load images from the directory
+    dataset = []
+    for directory in os.listdir(IMG_SAVE_PATH):
+        path = os.path.join(IMG_SAVE_PATH, directory)
+        if not os.path.isdir(path):
+            continue
+        for item in os.listdir(path):
+            # to make sure no hidden files get in our way
+            if item.startswith("."):
+                continue
+            img = cv2.imread(os.path.join(path, item))
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = cv2.resize(img, (227, 227))
+            dataset.append([img, directory])
+
+    '''
+    dataset = [
+        [[...], 'rock'],
+        [[...], 'paper'],
+        ...
+    ]
+    '''
+
+    data, labels = zip(*dataset)
+    labels = list(map(TrainingDataset.mapper, labels))
+
+    '''
+    labels: rock,paper,paper,scissors,rock...
+    one hot encoded: [1,0,0], [0,1,0], [0,1,0], [0,0,1], [1,0,0]...
+    '''
+    # one hot encode the labels
+    labels = np_utils.to_categorical(labels)
+
+    # define the model
+    model = TrainingDataset.get_model()
+    model.compile(
+        optimizer=Adam(lr=0.0001),
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    # start training
+    model.fit(np.array(data), np.array(labels), epochs=10)
+
+    # save the model for later use
+    model.save("rock-paper-scissors-model.h5")
 
 
 main()
